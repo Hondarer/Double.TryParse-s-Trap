0ef13a258724b2eb23ffc8c1d0530cf96a7164da
Tetsuo Honda <t-honda@hondarer-soft.com>
2017-04-01 11:46:10 +0900

    initial commit

---
+++ Double.TryParse-s-Trap/Double.TryParse-s-Trap/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Double.TryParse_s_Trap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = double.NaN;
+            Console.WriteLine(double.TryParse("1,2,,,,,,3", out result));
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
